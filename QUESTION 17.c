16828f18b0b1f172c000d3e242eaf19f9c891584
PRAGATIRAWAT09 <pragatirawat2309@gmail.com>
2025-11-18 19:36:04 +0530

    Create QUESTION 17.c

---
+++ QUESTION 17.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int size, i; //Program to find the largest element in an array.
+    int largest;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("Enter the elements of the array:");
+    for (i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    // Assume the first element is the largest
+    largest = arr[0];
+    for (i = 1; i < size; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    printf("The largest element in the array is: %d\n", largest);
+
+    return 0;
+}
