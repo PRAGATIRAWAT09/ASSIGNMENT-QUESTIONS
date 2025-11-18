9fd28a24e58876a7804fef0b68cde5d16732ce97
PRAGATIRAWAT09 <pragatirawat2309@gmail.com>
2025-11-18 19:18:31 +0530

    Add prime number checking program in QUESTION 15.c
    Implement a program to check if a number is prime using a function.

---
+++ QUESTION 15.c
@@ -0,0 +1,27 @@
+#include <stdio.h> //Program to check whether a number is prime uing function .
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0; 
+    }
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1; 
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (isPrime(num)) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+
+    return 0;
+}
